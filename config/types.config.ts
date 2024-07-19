37c360405bea621cec4247e24e7e39382badf02a
melonges <melonges@icloud.com>
2024-07-20 00:42:55 +0300

    feat: yaml config

---
+++ config/types.config.ts
@@ -0,0 +1,12 @@
+export type Config = {
+  initial_state: {
+    points: number;
+    energy: number;
+    ar: number;
+  };
+  limits: {
+    points: number;
+    energy: number;
+    referral: 48;
+  };
+};
