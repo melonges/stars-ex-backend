ad5b1d960818f548ea3797940f36c912b620830b
melonges <melonges@icloud.com>
2024-07-20 03:09:52 +0300

    cors any origin

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { bootstrapSwagger } from './common/swagger/bootstrap-swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: { origin: '*' } });
   bootstrapSwagger(app);
   await app.listen(3000);
 }
