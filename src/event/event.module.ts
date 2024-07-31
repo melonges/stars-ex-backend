ab49237d16cfa38670ae675de6b793f59d0c645b
melonges <melonges@icloud.com>
2024-07-31 19:29:19 +0300

    fix: dependency of `PlayerRepository`

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { EventService } from './event.service';
 import { EventController } from './event.controller';
-import { PlayerModule } from 'src/player/player.module';
 import { AssetModule } from 'src/asset/asset.module';
 import { ConfigModule } from '@nestjs/config';
 import configuration from 'config/configuration';
+import { Player } from 'src/player/entities/player.entity';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
-  imports: [PlayerModule, AssetModule, ConfigModule.forFeature(configuration)],
+  imports: [
+    AssetModule,
+    ConfigModule.forFeature(configuration),
+    MikroOrmModule.forFeature([Player]),
+  ],
   controllers: [EventController],
   providers: [EventService],
 })
