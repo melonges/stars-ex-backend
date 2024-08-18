a4f2885261f26a6c49631ee0c16e8867bce2c24b
melonges <melonges@icloud.com>
2024-08-18 15:16:53 +0300

    setup sentry

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import './common/instrument';
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { TelegramModule } from './telegram/telegram.module';
@@ -8,9 +9,11 @@ import { EventModule } from './event/event.module';
 import { SettingsModule } from './settings/settings.module';
 import { TasksModule } from './tasks/tasks.module';
 import { AssetsModule } from './assets/assets.module';
+import { SentryModule } from '@sentry/nestjs/setup';
 
 @Module({
   imports: [
+    SentryModule.forRoot(),
     AuthModule,
     TelegramModule,
     PlayerModule,
