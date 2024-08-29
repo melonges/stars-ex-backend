f2e90f48ecfd9bf1c35513d74596e1124394069d
melonges <melonges@icloud.com>
2024-08-29 21:48:48 +0300

    fix: disable refresh db in dev mode

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,17 +1,10 @@
 import { MikroORM } from '@mikro-orm/core';
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class DatabaseService implements OnModuleInit {
-  constructor(
-    private readonly orm: MikroORM,
-    private configService: ConfigService,
-  ) {}
+  constructor(private readonly orm: MikroORM) {}
   onModuleInit() {
-    if (this.configService.get('NODE_ENV') === 'development')
-      this.orm.schema.refreshDatabase();
-
     this.orm.getSchemaGenerator().updateSchema();
   }
 }
